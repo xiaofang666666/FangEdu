f7715721964ef28fdc89d3a6eaf0d6819ddafaf5
xiaofang <xiaofang66612019@outlook.com>
2021-02-20 17:33:49 +0800

    display of main page

--- FangEdu/apps/courses/models.py
+++ FangEdu/apps/courses/models.py
@@ -4,23 +4,24 @@ from orgs.models import OrgInfo,TeacherInfo
 
 # Create your models here.
 class CourseInfo(models.Model):
-    image = models.ImageField(upload_to='course/',max_length=200,verbose_name="课程封面")
-    name = models.CharField(max_length=20,verbose_name="课程名称")
-    study_time = models.IntegerField(default=0,verbose_name="学习时长")
-    study_num = models.IntegerField(default=0,verbose_name="学习人数")
-    level = models.CharField(choices=(('gj','高级'),('zj','中级'),('cj','初级')),max_length=5,verbose_name="课程难度",default='cj')
-    love_num = models.IntegerField(default=0,verbose_name="收藏数")
-    click_num = models.IntegerField(default=0,verbose_name="访问量")
-    desc = models.CharField(max_length=200,verbose_name="课程简介")
+    image = models.ImageField(upload_to='course/', max_length=200, verbose_name="课程封面")
+    name = models.CharField(max_length=20, verbose_name="课程名称")
+    study_time = models.IntegerField(default=0, verbose_name="学习时长")
+    study_num = models.IntegerField(default=0, verbose_name="学习人数")
+    level = models.CharField(choices=(('gj', '高级'), ('zj', '中级'), ('cj', '初级')), max_length=5, verbose_name="课程难度",
+                             default='cj')
+    love_num = models.IntegerField(default=0, verbose_name="收藏数")
+    click_num = models.IntegerField(default=0, verbose_name="访问量")
+    desc = models.CharField(max_length=200, verbose_name="课程简介")
     detail = models.TextField(verbose_name="课程详情")
-    catagory = models.CharField(choices=(('qd','前端开发'),('hd','后端开发')),verbose_name="课程类别",max_length=5)
-    course_notice = models.CharField(max_length=200,verbose_name="课程公告")
-    course_need = models.CharField(max_length=100,verbose_name="课程须知")
-    teacher_tell = models.CharField(max_length=100,verbose_name="老师教导")
-    orginfo = models.ForeignKey(OrgInfo,verbose_name="所属机构",on_delete=models.CASCADE)
-    teacherinfo = models.ForeignKey(TeacherInfo,verbose_name="所属讲师",on_delete=models.CASCADE)
+    catagory = models.CharField(choices=(('qd', '前端开发'), ('hd', '后端开发')), verbose_name="课程类别", max_length=5)
+    course_notice = models.CharField(max_length=200, verbose_name="课程公告")
+    course_need = models.CharField(max_length=100, verbose_name="课程须知")
+    teacher_tell = models.CharField(max_length=100, verbose_name="老师教导")
+    orginfo = models.ForeignKey(OrgInfo, verbose_name="所属机构", on_delete=models.CASCADE)
+    teacherinfo = models.ForeignKey(TeacherInfo, verbose_name="所属讲师", on_delete=models.CASCADE)
     is_banner = models.BooleanField(default=False, verbose_name="是否轮播")
-    add_time = models.DateTimeField(default=datetime.now,verbose_name="添加时间")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name="添加时间")
 
     def __str__(self):
         return self.name
